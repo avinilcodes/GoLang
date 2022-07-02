a0070ef112bb85d49cbf5dce287d12d6b28e8f47
Avinil.Bedarkar <avinilbedarkar@gmail.com>
2022-07-02 17:21:21 +0530

    Day1 - Constants

---
+++ 05_constants.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const a int = 9
+	fmt.Println("Values of a:", a)
+}
+
+//Constant has to be known at compile time and should not be reinitialized
