94abed789c13cdc976f83cf50dbb480b674f98b1
Avinil.Bedarkar <avinilbedarkar@gmail.com>
2022-07-02 22:34:32 +0530

    Day1 -anonymous Functions

--- 08_anonymous_functions.go
+++ 08_anonymous_functions.go
@@ -6,6 +6,17 @@ import (
 
 func main() {
 	func() { // this function is anonymous function
-		fmt.Println("Inside a anonymous function")
+		fmt.Println("Inside anonymous function")
 	}()
+
+	s := func(a, b int) int { // this function is anonymous function
+		fmt.Println("Inside another anonymous function")
+		return a + b
+	}
+	fmt.Println("Sum of 3,2 is :", s(2, 3))
+
+	f := func(a, b int) int { // this function is anonymous function
+		return a - b
+	}(5, 2)
+	fmt.Println("diff between 5,2 is: ", f)
 }
