5ce334e945598009da2d1b120c299429c00bf0cd
avinilcodes <avinilbedarkar@gmail.com>
2022-07-07 21:44:04 +0530

    Go -introduction

---
+++ Excercise/Go-Introduction/01_multiplication.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int64 = 32132
+	var b int64 = 42452
+
+	fmt.Println("multiplication of a ", a, " and b", b, " is ", a*b)
+}
---
+++ Excercise/Go-Introduction/03_variables.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//declaring variable using var
+	var a int = 25
+	//shorthand declaration
+	b := 34
+
+	fmt.Println("a is", a, "b is", b)
+}
