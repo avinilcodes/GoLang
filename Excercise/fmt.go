acecf7d8aafccdd23b191cbc551c06000cf92e97
avinilcodes <avinilbedarkar@gmail.com>
2022-07-11 11:16:22 +0530

    fmt

--- Excercise/fmt.go
+++ Excercise/fmt.go
@@ -8,5 +8,9 @@ func main() {
 	v := 1
 	p := &v
 	s := "THis is a string"
+	f := 45.23
 	fmt.Printf("%#v %#v %#v", v, p, s)
+	fmt.Printf("%p %s", p, s)
+	fmt.Printf("%d", v)
+	fmt.Printf("%g", f)
 }
