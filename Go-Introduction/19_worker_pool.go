5b84559f03e6ef6616e6f23f2b409be0a5cad623
avinilcodes <avinilbedarkar@gmail.com>
2022-09-28 10:01:49 +0530

    Buffered chan and worker pool

---
+++ Go-Introduction/19_worker_pool.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int) { //jobs is receiver channel and results is sender channel
+	for j := range jobs {
+		fmt.Println("Worker ", id, "Started job ", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker ", id, "Finished ", j)
+		results <- j * 2
+	}
+}
+
+func main() {
+
+	const numJobs = 5
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	for i := 1; i <= numJobs; i++ {
+		r := <-results
+		fmt.Println(r)
+	}
+
+}
+
+// Irrespective of jobs sequence we want to get the task/job/computation done in background
+// we use worker pool.
